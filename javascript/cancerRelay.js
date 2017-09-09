510ce1585e5a353beeb3cfe6579f87efebaf3ec3
Scott Splavec <scott.splavec@gmail.com>
2017-09-09 00:39:27 -0500

    Removed commented code

--- javascript/cancerRelay.js
+++ javascript/cancerRelay.js
@@ -1,31 +1,20 @@
 $(document).ready(function(){
-//  var divFocus = '';
 
   $(window).keypress(function(e){
     var x = e.which || e.keycode;
     if (x > 48 && x <= 57) {
-      addItUp(x - 48);//, divFocus);
+      addItUp(x - 48);
     }
   });
 
   $('#teamTotalDiv').click(function(){
-    //var teamTotal = Number($('#teamTotalCount').text());
-    //$('#teamTotalCount').text(teamTotal + 1);
-    //addItUp(1);
     $('#teamTotalDiv').addClass('focus');
     $('#townTotalDiv').removeClass('focus');
-//    divFocus = 'team';
-//    increaseCount($('#teamTotalCount'));
   });
 
   $('#townTotalDiv').click(function(){
-    //var townTotal = Number($('#townTotalCount').text());
-    //$('#townTotalCount').text(townTotal + 1);
-    //addItUp(1);
     $('#townTotalDiv').addClass('focus');
     $('#teamTotalDiv').removeClass('focus');
-//    divFocus = 'town';
-//    increaseCount($('#townTotalCount'));
   });
 });
 
