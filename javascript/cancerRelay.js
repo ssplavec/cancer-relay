eea1352d669bd5aaa865e381002482d8e8aa886b
Scott Splavec <scott.splavec@gmail.com>
2017-10-19 11:08:36 -0500

    Latest changes

--- javascript/cancerRelay.js
+++ javascript/cancerRelay.js
@@ -1,49 +1,100 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-  $(window).keypress(function(e){
-    var x = e.which || e.keycode;
-    if (x > 48 && x <= 57) {
-      addItUp(x - 48);
+    if (typeof(Storage) !== undefined) {
+        $('#teamTotalCount').text(localStorage.teamCount);
+        $('#townTotalCount').text(localStorage.townCount);
+        $('#totalCount').text(localStorage.totalCount);
     }
-  });
 
-  $('#teamTotalDiv').click(function(){
-    $('#teamTotalDiv').addClass('focus');
-    $('#townTotalDiv').removeClass('focus');
-  });
+    $(window).keypress(function(e) {
+        var x = e.which || e.keycode;
+        if (x > 48 && x <= 57) {
+          addItUp(x - 48);
+        }
+    });
 
-  $('#townTotalDiv').click(function(){
-    $('#townTotalDiv').addClass('focus');
-    $('#teamTotalDiv').removeClass('focus');
-  });
+    $('#teamTotalDiv').click(function() {
+        $('#teamTotalDiv').addClass('focus');
+        $('#townTotalDiv').removeClass('focus');
+    });
+
+    $('#townTotalDiv').click(function(){
+        $('#townTotalDiv').addClass('focus');
+        $('#teamTotalDiv').removeClass('focus');
+    });
+
+    $('#logoDiv').click(function(){
+        if (typeof(Storage) !== undefined) {
+          localStorage.teamCount = 0;
+          localStorage.townCount = 0;
+          localStorage.totalCount = 0;
+        }
+    });
 });
 
-addItUp = function(entry){
-  if ($('#teamTotalDiv').hasClass('focus')) {
-    incrementTeamCount(entry);
-  } else if($('#townTotalDiv').hasClass('focus')) {
-    incrementTownCount(entry);
-  } else {
-    return;
-  }
-  incrementTotalCount(entry);
+var addItUp = function(entry) {
+    if ($('#teamTotalDiv').hasClass('focus')) {
+        incrementTeamCount(entry);
+    } else if ($('#townTotalDiv').hasClass('focus')) {
+        incrementTownCount(entry);
+    } else {
+        return;
+    }
+    incrementTotalCount(entry);
 };
 
-incrementTeamCount = function(entry) {
+var incrementTeamCount = function(entry) {
     increaseCount($('#teamTotalCount'), entry);
+    storeCount('team', entry);
 };
 
-incrementTownCount = function(entry) {
+var incrementTownCount = function(entry) {
     increaseCount($('#townTotalCount'), entry);
+    storeCount('town', entry);
 };
 
-incrementTotalCount = function(entry) {
-  var total = Number($('#totalCount').text());
-  var newTotal = total + entry;
-  $('#totalCount').text(newTotal);
+var incrementTotalCount = function(entry) {
+  increaseCount($('#totalCount'), entry);
+  storeCount('total', entry);
 };
 
-increaseCount = function(whichCounter, entry) {
-  var totalCount = Number(whichCounter.text());
-  whichCounter.text(totalCount + entry);
+var increaseCount = function(whichCounter, entry) {
+    var totalCount = Number(whichCounter.text());
+    whichCounter.text(totalCount + entry);
+};
+
+var storeCount = function(whichCounter, value) {
+    if (typeof(Storage) !== undefined) {
+        if ('team' === whichCounter) {
+          storeTeamCount(value);
+        } else if ('town' === whichCounter) {
+          storeTownCount(value);
+        } else if ('total' === whichCounter) {
+          storeTotalCount(value);
+        }
+    }
+};
+
+var storeTeamCount = function(value) {
+    if (localStorage.teamCount) {
+      localStorage.teamCount = Number(localStorage.teamCount) + value;
+    } else {
+      localStorage.teamCount = value;
+    }
+};
+
+var storeTownCount = function(value) {
+    if (localStorage.townCount) {
+        localStorage.townCount = Number(localStorage.townCount) + value;
+    } else {
+        localStorage.townCount = value;
+    }
+};
+
+var storeTotalCount = function(value) {
+    if (localStorage.totalCount) {
+        localStorage.totalCount = Number(localStorage.totalCount) + value;
+    } else {
+        localStorage.totalCount = value;
+    }
 };
